34e96c724bf41a8f0c8588d432c6dcdcb8dd6805
Vadosdavos <bezbvap@gmail.com>
2021-09-14 22:00:56 +0200

    fix: fix form select

--- museum/src/form.js
+++ museum/src/form.js
@@ -2,13 +2,20 @@ const form = document.querySelector('.form-wrapper');
 const buyNow = document.querySelector('.buy-btn');
 const formClose = document.querySelector('.form-close');
 const overlay = document.querySelector('.overlay');
+const formSelect = document.querySelector('.form-select');
 
 buyNow.addEventListener('click', function () {
   form.classList.add('form-visible');
   overlay.classList.add('overlay-visible');
 });
-
 formClose.addEventListener('click', function () {
   form.classList.remove('form-visible');
   overlay.classList.remove('overlay-visible');
+});
+overlay.addEventListener('click', function () {
+  form.classList.remove('form-visible');
+  overlay.classList.remove('overlay-visible');
+});
+formSelect.addEventListener('click', function () {
+  formSelect.classList.toggle('form-select-active');
 });
