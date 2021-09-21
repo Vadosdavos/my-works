8dd254b0fb022b72c960765d622b5a65d2e42ee2
Vadosdavos <bezbvap@gmail.com>
2021-09-21 17:57:38 +0200

    feat: add ripple to book-button

--- museum/src/index.js
+++ museum/src/index.js
@@ -2,3 +2,4 @@ import './style.css';
 import './explore';
 import './video';
 import './form';
+import './ripple';
--- museum/src/video.js
+++ museum/src/video.js
@@ -2,14 +2,14 @@ const range = document.querySelectorAll('input[type = range]');
 const play = document.querySelector('.play');
 const sound = document.querySelector('.sound');
 play.addEventListener('click', function () {
-    play.classList.toggle('stop');
+  play.classList.toggle('stop');
 });
 sound.addEventListener('click', function () {
-    sound.classList.toggle('sound-stop');
+  sound.classList.toggle('sound-stop');
 });
 range.forEach((e) => {
-    e.addEventListener('input', function () {
+  e.addEventListener('input', function () {
     const value = this.value;
-    this.style.background = `linear-gradient(to right, #24809e 0%, #24809e ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
-    });
+    this.style.background = `linear-gradient(to right, var(--dark-red) 0%, var(--dark-red) ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+  });
 });
