7dfee4740434cb0f3a1a2cd55465644bf47d293e
Vadosdavos <bezbvap@gmail.com>
2021-09-27 12:39:41 +0200

    refactor: change time and date elements in form

--- museum-adaptive/src/form.js
+++ museum-adaptive/src/form.js
@@ -3,6 +3,10 @@ const buyNow = document.querySelector('.buy-btn');
 const formClose = document.querySelector('.form-close');
 const overlay = document.querySelector('.overlay');
 const formSelect = document.querySelector('.form-select');
+const formDate = document.querySelector('.date');
+const formDatePsewdo = document.querySelector('.date-psewdo');
+const formTime = document.querySelector('.time');
+const formTimePsewdo = document.querySelector('.time-psewdo');
 
 buyNow.addEventListener('click', function () {
   form.classList.add('form-visible');
@@ -18,4 +22,22 @@ overlay.addEventListener('click', function () {
 });
 formSelect.addEventListener('click', function () {
   formSelect.classList.toggle('form-select-active');
+});
+formDate.addEventListener('click', () => {
+  formDatePsewdo.classList.add('date-psewdo-nonvisible');
+});
+formDate.addEventListener('blur', () => {
+  formDatePsewdo.classList.remove('date-psewdo-nonvisible');
+  if (formDate.value) {
+    formDatePsewdo.innerHTML = `${formDate.value}`;
+  } else formDatePsewdo.innerHTML = `Date`;
+});
+formTime.addEventListener('click', () => {
+  formTimePsewdo.classList.add('time-psewdo-nonvisible');
+});
+formTime.addEventListener('blur', () => {
+  formTimePsewdo.classList.remove('time-psewdo-nonvisible');
+  if (formTime.value) {
+    formTimePsewdo.innerHTML = `${formTime.value}`;
+  } else formTimePsewdo.innerHTML = `Time`;
 });
