c39fc2a56470cb8b11fe52a07de3cd66b007d14f
Vadosdavos <bezbvap@gmail.com>
2021-09-29 17:57:09 +0200

    feat: add adaptive layout (768px)

--- museum-adaptive/src/menu.js
+++ museum-adaptive/src/menu.js
@@ -1,8 +1,10 @@
 const burgerContainer = document.querySelector('.burger-container');
 const burger = document.querySelector('.burger');
-const menu = document.querySelector('.header-nav');
+const menu = document.querySelector('.header-nav-container');
+const welcomeTextContainer = document.querySelector('.welcome-text-container');
 
 burgerContainer.addEventListener('click', () => {
   burger.classList.toggle('burger-open');
   menu.classList.toggle('header-nav-open');
+  welcomeTextContainer.classList.toggle('opacity-zero');
 });
