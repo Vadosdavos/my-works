0a3b175e8e45cffac2621073725dc3d15c2566e9
Vadosdavos <bezbvap@gmail.com>
2021-11-14 17:38:19 +0200

    feat: add pictures to questions

--- art-quiz/components/question-page/question-page.ts
+++ art-quiz/components/question-page/question-page.ts
@@ -30,5 +30,45 @@ export class QuestionPage extends BaseComponent {
     this.element.append(this.categoriesButton.element);
     this.questionWrapper = new BaseComponent('div', ['question-wrapper']);
     this.element.append(this.questionWrapper.element);
+    if (this.type === CategoriesTypes.artists) {
+      this.questionWrapper.element.append(...this.createArtistsQuestions());
+    } else if (this.type === CategoriesTypes.pictures) {
+      this.questionWrapper.element.append(this.createPicturesQuestions());
+    }
+  }
+
+  createArtistsQuestions() {
+    const artistsImageContainer = new BaseComponent('div', [
+      'artist-image-container',
+    ]);
+    const artistsImage = new BaseComponent<HTMLImageElement>('img', [
+      'artist-image',
+    ]);
+    artistsImage.element.src = './image1.jpg';
+    artistsImage.element.alt = 'Question image';
+    artistsImageContainer.element.append(artistsImage.element);
+    const artistAnswersContainer = new BaseComponent('div', [
+      'artist-answers-container',
+    ]);
+    for (let i = 0; i < 4; i++) {
+      artistAnswersContainer.element.append(
+        new BaseComponent('p', ['artist-answers'], `test-${i}`).element
+      );
+    }
+    return [artistsImageContainer.element, artistAnswersContainer.element];
+  }
+  createPicturesQuestions() {
+    const picturesImageContainer = new BaseComponent('div', [
+      'pictures-image-container',
+    ]);
+    for (let i = 0; i < 4; i++) {
+      const picturesImage = new BaseComponent<HTMLImageElement>('img', [
+        'pictures-image',
+      ]);
+      picturesImage.element.src = `./image${i + 3}.jpg`;
+      picturesImage.element.alt = 'Answer Option image';
+      picturesImageContainer.element.append(picturesImage.element);
+    }
+    return picturesImageContainer.element;
   }
 }
