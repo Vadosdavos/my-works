52a76693d2cab1a89c87c2bd4be856cd9504d5f7
Vadosdavos <bezbvap@gmail.com>
2021-12-01 23:12:31 +0200

    feat: setup typeScript and eslint

---
+++ migration-to-typescript/src/index.ts
@@ -0,0 +1,7 @@
+// import App from './components/app/app';
+// import './global.css';
+
+// const app = new App();
+// app.start();
+
+console.log('hello');
--- migration-to-typescript/webpack.config.js
+++ migration-to-typescript/webpack.config.js
@@ -1,77 +1,43 @@
 const path = require('path');
+const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
 
-const devServer = (isDev) =>
-  !isDev
-    ? {}
-    : {
-        devServer: {
-          open: true,
-          hot: true,
-          port: 8080,
-          static: path.join(__dirname, 'public'),
-        },
-      };
-
-const esLintPlugin = (isDev) =>
-  isDev ? [] : [new ESLintPlugin({ extensions: ['ts', 'js'] })];
-
-module.exports = ({ development }) => ({
-  mode: development ? 'development' : 'production',
-  devtool: development ? 'inline-source-map' : false,
-  entry: {
-    main: './src/index.ts',
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'assets/[hash][ext]',
-  },
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  mode: 'development',
   module: {
     rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
       {
         test: /\.[tj]s$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
     ],
   },
-  plugins: [
-    ...esLintPlugin(development),
-    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
-    new HtmlWebpackPlugin({ template: './src/index.html' }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: 'public',
-          noErrorOnMissing: true,
-        },
-      ],
-    }),
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-  ],
   resolve: {
     extensions: ['.ts', '.js'],
   },
-  ...devServer(development),
-});
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
