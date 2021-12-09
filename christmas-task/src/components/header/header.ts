9e5a99bace549cdd6ea8a820674c5cb81a27b317
Vadosdavos <bezbvap@gmail.com>
2021-12-09 16:04:34 +0200

    feat: add layout and hover-style to header

--- christmas-task/src/components/header/header.ts
+++ christmas-task/src/components/header/header.ts
@@ -5,13 +5,15 @@ export class Header extends BaseComponent {
   logo = new BaseComponent('span', ['logo']);
   toysButton = new BaseComponent('span', ['toys-button'], 'Игрушки');
   treeButton = new BaseComponent('span', ['tree-button'], 'Ёлка');
+  headerNav = new BaseComponent('nav', ['header-nav']);
 
   constructor() {
     super('header', ['main-header']);
-    this.element.append(
+    this.headerNav.element.append(
       this.logo.element,
       this.toysButton.element,
       this.treeButton.element
     );
+    this.element.append(this.headerNav.element);
   }
 }
