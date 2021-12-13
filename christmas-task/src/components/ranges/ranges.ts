d610fe20a908c3ff3097d875aac590e2f1c6510b
Vadosdavos <bezbvap@gmail.com>
2021-12-13 17:55:10 +0200

    feat: start range-filter

--- christmas-task/src/components/ranges/ranges.ts
+++ christmas-task/src/components/ranges/ranges.ts
@@ -2,7 +2,19 @@ import { BaseComponent } from '../base-componet';
 import './ranges.scss';
 
 export class Ranges extends BaseComponent {
+  title = new BaseComponent('h3', ['filters-title'], 'Фильтры по диапазону');
+  amount = new BaseComponent('div', ['amout-container'], 'amout');
+  year = new BaseComponent('div', ['year-container'], 'year');
+
   constructor() {
-    super('div', ['ranges'], 'ranges');
+    super('div', ['ranges']);
+    this.render();
+  }
+  render() {
+    this.element.append(
+      this.title.element,
+      this.amount.element,
+      this.year.element
+    );
   }
 }
