6d1c9c66cd2e2955cda78b324998d37751b35d0f
Vadosdavos <bezbvap@gmail.com>
2021-12-13 17:25:01 +0200

    feat: add filters and input-component

--- christmas-task/src/components/filters/filters.ts
+++ christmas-task/src/components/filters/filters.ts
@@ -1,8 +1,46 @@
 import { BaseComponent } from '../base-componet';
+import { InputComponent, InputTypes } from '../input-component';
 import './filters.scss';
 
 export class Filters extends BaseComponent {
+  title = new BaseComponent('h3', ['filters-title'], 'Фильтры по назначению');
+  shapeFilter = new BaseComponent('div', ['shape-filter']);
+  colorFilter = new BaseComponent('div', ['color-filter']);
+  sizeFilter = new BaseComponent('div', ['size-filter'], 'Размер:');
+  favoriteFilter = new InputComponent(
+    InputTypes.checkbox,
+    ['favorite-filter'],
+    'Только любимые:'
+  );
+
   constructor() {
-    super('div', ['filters'], 'filters');
+    super('div', ['filters']);
+    this.render();
+  }
+
+  render() {
+    this.shapeFilter.element.innerHTML = `Форма:  
+    <button class="ball" data-filter="шар"></button>
+    <button class="bell" data-filter="колокольчик"></button>
+    <button class="cone" data-filter="шишка"></button>
+    <button class="snowflake" data-filter="снежинка"></button>
+    <button class="figure" data-filter="фигурка"></button>`;
+    this.colorFilter.element.innerHTML = `Цвет:   
+    <button class="filter-color-white" data-filter="белый"></button>
+    <button class="filter-color-yellow" data-filter="желтый"></button>
+    <button class="filter-color-red" data-filter="красный"></button>
+    <button class="filter-color-blue" data-filter="синий"></button>
+    <button class="filter-color-green" data-filter="зелёный"></button>`;
+    this.sizeFilter.element.innerHTML = `Размер: 
+    <button data-filter="большой" class=""></button>
+    <button data-filter="средний" class=""></button>
+    <button data-filter="малый" class=""></button>`;
+    this.element.append(
+      this.title.element,
+      this.shapeFilter.element,
+      this.colorFilter.element,
+      this.sizeFilter.element,
+      this.favoriteFilter.element
+    );
   }
 }
---
+++ christmas-task/src/components/input-component.ts
@@ -0,0 +1,26 @@
+export enum InputTypes {
+  text = 'text',
+  checkbox = 'checkbox',
+  range = 'range',
+  radio = 'radio',
+}
+
+export class InputComponent {
+  readonly element: HTMLInputElement | HTMLLabelElement;
+
+  constructor(type: InputTypes, styles: string[] = [], label?: string) {
+    if (label) {
+      this.element = document.createElement('label');
+      this.element.classList.add('input-label');
+      this.element.textContent = label;
+      const inputElement = document.createElement('input');
+      inputElement.setAttribute('type', type);
+      inputElement.classList.add(...styles);
+      this.element.append(inputElement);
+    } else {
+      this.element = document.createElement('input');
+      this.element.setAttribute('type', type);
+      this.element.classList.add(...styles);
+    }
+  }
+}
